7ec7ced81ca0939e9e958f0b2e43f9bb8b2f9e1b
Junnos  <junnos.dev@gmail.com>
2024-07-04 15:44:23 +0900

    ✨ Fixed infer to use multi-GPU

--- infer/__main__.py
+++ infer/__main__.py
@@ -1,3 +1,6 @@
+import logging
+import time
+
 from peft import PeftConfig, PeftModel
 from transformers import (
     AutoTokenizer,
@@ -10,7 +13,13 @@ import torch
 from train.model import Models
 from train.utils import SYSTEM_PROMPT
 
+_LOGGER = logging.getLogger(__name__)
+
+_DEVICE_MAP = "balanced"
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model_name = Models.EEVE_10_8B.value
@@ -19,13 +28,14 @@ if __name__ == "__main__":
     base_model = AutoModelForCausalLM.from_pretrained(
         model_name,
         quantization_config=bnb_config,
-        trust_remote_code=True,
         low_cpu_mem_usage=True,
+        device_map=_DEVICE_MAP,
     )
-    base_model.eval()
     base_model.config.use_cache = False
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name, trust_remote_code=True, device_map=_DEVICE_MAP
+    )
     tokenizer.pad_token = tokenizer.eos_token
 
     adapter_path = "res/lora"
@@ -33,36 +43,40 @@ if __name__ == "__main__":
     peft_config = PeftConfig.from_pretrained(adapter_path)
 
     adapted_model = PeftModel.from_pretrained(
-        model=base_model, model_id=adapter_path, adapter_name="lora", config=peft_config
+        model=base_model,
+        model_id=adapter_path,
+        adapter_name="lora",
+        config=peft_config,
+        is_trainable=False,
+        device_map=_DEVICE_MAP,
     )
     adapted_model.set_adapter("lora")
-    adapted_model.to(device)
-    adapted_model.eval()
-    print(adapted_model.active_adapter)
+    _LOGGER.info(f"Using adapter: {adapted_model.active_adapter}")
+
+    adapted_model.generation_config.cache_implementation = "static"
+
+    compiled_model = torch.compile(
+        adapted_model, mode="reduce-overhead", fullgraph=True
+    )
+    compiled_model.eval()
 
     adapted_pipeline = pipeline(
         "text-generation",
-        model=adapted_model,
+        model=compiled_model,
         tokenizer=tokenizer,
         model_kwargs={"torch_dtype": torch.bfloat16},
-        device_map="auto",
-    )
-
-    base_pipeline = pipeline(
-        "text-generation",
-        model=base_model,
-        tokenizer=tokenizer,
-        model_kwargs={"torch_dtype": torch.bfloat16},
-        device_map="auto",
+        device_map=_DEVICE_MAP,
     )
 
     terminators = [
         adapted_pipeline.tokenizer.eos_token_id,
-        adapted_pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>"),
+        adapted_pipeline.tokenizer.convert_tokens_to_ids("<|im_end|>"),
     ]
 
     while True:
         text = input("☘️ ")
+
+        start = time.time()
 
         prompt = adapted_pipeline.tokenizer.apply_chat_template(
             [
@@ -75,21 +89,14 @@ if __name__ == "__main__":
 
         adapted_outputs = adapted_pipeline(
             prompt,
-            max_new_tokens=2048,
+            max_new_tokens=256,
             eos_token_id=terminators,
             do_sample=True,
             temperature=0.6,
             top_p=0.9,
         )
 
-        base_outputs = base_pipeline(
-            prompt,
-            max_new_tokens=2048,
-            eos_token_id=terminators,
-            do_sample=True,
-            temperature=0.6,
-            top_p=0.9,
-        )
+        end = time.time()
 
-        print(adapted_outputs[0]["generated_text"][len(prompt) :])
-        print(base_outputs[0]["generated_text"][len(prompt) :])
+        _LOGGER.info(adapted_outputs[0]["generated_text"][len(prompt) :])
+        _LOGGER.info(f"Inference time: {end - start:.5f} sec.")
